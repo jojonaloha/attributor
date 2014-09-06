074bb6c6687cf2a5f74c0fb95238ac90c505ecf0
Jonathan Jordan <jonathan@metaltoad.com>
2014-09-06 06:51:46 -0700

    Demo lecture 5.

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,21 +9,68 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
 @implementation AttributorViewController
 
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute: NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage addAttributes:@{NSStrokeColorAttributeName:[UIColor blackColor],
+                                           NSStrokeWidthAttributeName:@-2}
+                                   range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3,
+                           NSStrokeColorAttributeName: self.outlineButton.tintColor}
+                   range:NSMakeRange(0, [title length])];
+     [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification {
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts {
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
 }
 
 @end
